8ed662321d9417b065c3cfc8b5685c6c8cc555c8
Alex Gleason <alex@alexgleason.me>
2023-03-04 20:59:39 -0600

    Implement apps API (the stupid way)

---
+++ src/api/apps.ts
@@ -0,0 +1,21 @@
+import type { Context } from '@/deps.ts';
+
+const FAKE_APP = {
+  id: '1',
+  name: 'Nostrverse',
+  website: null,
+  redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+  client_id: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+  client_secret: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+  vapid_key: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=',
+};
+
+function createAppController(c: Context) {
+  return c.json(FAKE_APP);
+}
+
+function appVerifyCredentials(c: Context) {
+  return c.json(FAKE_APP);
+}
+
+export { appVerifyCredentials, createAppController };
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import { Hono } from '@/deps.ts';
+import { appVerifyCredentials, createAppController } from './api/apps.ts';
 
 import instanceController from './api/instance.ts';
 
 const app = new Hono();
 
 app.get('/api/v1/instance', instanceController);
+
+app.get('/api/v1/apps/verify_credentials', appVerifyCredentials);
+app.post('/api/v1/apps', createAppController);
 
 export default app;
