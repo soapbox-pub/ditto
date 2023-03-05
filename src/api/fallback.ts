2ab9e60048080b80de85646bdf6bd0c8bacd661b
Alex Gleason <alex@alexgleason.me>
2023-03-05 00:36:37 -0600

    Add additional fallback controllers

--- src/api/fallback.ts
+++ src/api/fallback.ts
@@ -1,5 +1,6 @@
 import type { Context } from '@/deps.ts';
 
 const emptyArrayController = (c: Context) => c.json([]);
+const emptyObjectController = (c: Context) => c.json({});
 
-export { emptyArrayController };
+export { emptyArrayController, emptyObjectController };
--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { cors, Hono } from '@/deps.ts';
 
 import { credentialsController } from './api/accounts.ts';
 import { appCredentialsController, createAppController } from './api/apps.ts';
-import { emptyArrayController } from './api/fallback.ts';
+import { emptyArrayController, emptyObjectController } from './api/fallback.ts';
 import instanceController from './api/instance.ts';
 import { createTokenController } from './api/oauth.ts';
 import { createStatusController } from './api/statuses.ts';
@@ -17,6 +17,7 @@ app.get('/api/v1/apps/verify_credentials', appCredentialsController);
 app.post('/api/v1/apps', createAppController);
 
 app.post('/oauth/token', createTokenController);
+app.post('/oauth/revoke', emptyObjectController);
 
 app.get('/api/v1/accounts/verify_credentials', credentialsController);
 
@@ -24,7 +25,15 @@ app.post('/api/v1/statuses', createStatusController);
 
 // Not (yet) implemented.
 app.get('/api/v1/timelines/*', emptyArrayController);
+app.get('/api/v1/notifications', emptyArrayController);
 app.get('/api/v1/accounts/:id/statuses', emptyArrayController);
 app.get('/api/v1/bookmarks', emptyArrayController);
+app.get('/api/v1/custom_emojis', emptyArrayController);
+app.get('/api/v1/accounts/search', emptyArrayController);
+app.get('/api/v2/search', (c) => c.json({ accounts: [], statuses: [], hashtags: [] }));
+app.get('/api/v1/filters', emptyArrayController);
+app.get('/api/v1/blocks', emptyArrayController);
+app.get('/api/v1/mutes', emptyArrayController);
+app.get('/api/v1/domain_blocks', emptyArrayController);
 
 export default app;
