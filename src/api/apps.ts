46646090472fac10ea0573bf2c4d638bdb043ef8
Alex Gleason <alex@alexgleason.me>
2023-04-10 21:13:57 -0500

    apps: add comments

--- src/api/apps.ts
+++ src/api/apps.ts
@@ -1,12 +1,16 @@
 import type { Context } from '@/deps.ts';
 
+/**
+ * Apps are unnecessary cruft in Mastodon API, but necessary to make clients work.
+ * So when clients try to "create" an app, pretend they did and return a hardcoded app.
+ */
 const FAKE_APP = {
   id: '1',
   name: 'Ditto',
   website: null,
   redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
-  client_id: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-  client_secret: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+  client_id: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', // he cry
+  client_secret: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', // 😱 😱 😱
   vapid_key: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=',
 };
 
