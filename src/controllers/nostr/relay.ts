3593d5420d1b24cde73faa9e36f2e7ac0378ccb5
Alex Gleason <alex@alexgleason.me>
2023-08-12 14:01:30 -0500

    Relay: limit to 100 events per filter

--- src/controllers/nostr/relay.ts
+++ src/controllers/nostr/relay.ts
@@ -5,12 +5,15 @@ import { clientMsgSchema, type ClientREQ } from '@/schemas/nostr.ts';
 
 import type { AppController } from '@/app.ts';
 
+/** Limit of events returned per-filter. */
+const FILTER_LIMIT = 100;
+
 function connectStream(socket: WebSocket) {
   socket.onmessage = (e) => {
     const result = jsonSchema.pipe(clientMsgSchema).safeParse(e.data);
 
     if (!result.success) {
-      socket.send(JSON.stringify(['NOTICE', JSON.stringify(result.error.message)]));
+      socket.send(JSON.stringify(['NOTICE', 'Invalid message.']));
       return;
     }
 
@@ -27,11 +30,18 @@ function connectStream(socket: WebSocket) {
   };
 
   async function handleReq([_, sub, ...filters]: ClientREQ) {
-    for (const event of await getFilters(filters as Filter[])) {
+    for (const event of await getFilters(prepareFilters(filters))) {
       socket.send(JSON.stringify(['EVENT', sub, event]));
     }
     socket.send(JSON.stringify(['EOSE', sub]));
   }
+}
+
+function prepareFilters(filters: ClientREQ[2][]): Filter[] {
+  return filters.map((filter) => ({
+    ...filter,
+    limit: Math.min(filter.limit || FILTER_LIMIT, FILTER_LIMIT),
+  }));
 }
 
 const relayController: AppController = (c) => {
