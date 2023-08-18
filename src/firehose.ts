85345bc1576c6a1d82e6df3d69b107e57613648f
Alex Gleason <alex@alexgleason.me>
2023-08-17 20:28:23 -0500

    firehose: catch pipeline crash

--- src/firehose.ts
+++ src/firehose.ts
@@ -21,5 +21,8 @@ pool.subscribe(
 /** Handle events through the firehose pipeline. */
 function handleEvent(event: Event): Promise<void> {
   console.info(`firehose: Event<${event.kind}> ${event.id}`);
-  return pipeline.handleEvent(event);
+
+  return pipeline
+    .handleEvent(event)
+    .catch(() => {});
 }
