9cda8e3000e1b5520ca3fcf459c83ff3dc312c1f
Alex Gleason <alex@alexgleason.me>
2023-09-11 04:07:54 -0500

    csp: use template literals to avoid escaping single quotes

--- src/middleware/csp.ts
+++ src/middleware/csp.ts
@@ -8,17 +8,17 @@ const csp = (): AppMiddleware => {
 
     const policies = [
       'upgrade-insecure-requests',
-      'script-src \'self\'',
+      `script-src 'self'`,
       `connect-src 'self' blob: ${Conf.localDomain} ${wsProtocol}//${host}`,
       `media-src 'self' ${Conf.mediaDomain}`,
       `img-src 'self' data: blob: ${Conf.mediaDomain}`,
-      'default-src \'none\'',
-      'base-uri \'self\'',
-      'frame-ancestors \'none\'',
-      'style-src \'self\' \'unsafe-inline\'',
-      'font-src \'self\'',
-      'manifest-src \'self\'',
-      'frame-src \'self\' https:',
+      `default-src 'none'`,
+      `base-uri 'self'`,
+      `frame-ancestors 'none'`,
+      `style-src 'self' 'unsafe-inline'`,
+      `font-src 'self'`,
+      `manifest-src 'self'`,
+      `frame-src 'self' https:`,
     ];
 
     c.res.headers.set('content-security-policy', policies.join('; '));
