079cdcf7d720e957caf0cbf661fe62c94bb48fae
Alex Gleason <alex@alexgleason.me>
2023-10-11 23:48:01 -0500

    deno lint

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
-import { DenoSqlite3, DenoSqliteDialect, FileMigrationProvider, Kysely, Migrator, sql } from '@/deps.ts';
+import { DenoSqlite3, DenoSqliteDialect, FileMigrationProvider, Kysely, Migrator } from '@/deps.ts';
 import { Conf } from '@/config.ts';
 import { getPragma, setPragma } from '@/pragma.ts';
 
