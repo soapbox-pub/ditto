c532f54ea69653411b935b63d01ab8ccf4c262eb
Alex Gleason <alex@alexgleason.me>
2023-12-06 14:49:01 -0600

    Track queries with Sentry

--- src/workers/sqlite.worker.ts
+++ src/workers/sqlite.worker.ts
@@ -1,6 +1,7 @@
 /// <reference lib="webworker" />
 
-import { Comlink, type CompiledQuery, DenoSqlite3, type QueryResult } from '@/deps.ts';
+import { Comlink, type CompiledQuery, DenoSqlite3, type QueryResult, Sentry } from '@/deps.ts';
+import '@/sentry.ts';
 
 let db: DenoSqlite3 | undefined;
 
@@ -10,11 +11,16 @@ export const SqliteWorker = {
   },
   executeQuery<R>({ sql, parameters }: CompiledQuery): QueryResult<R> {
     if (!db) throw new Error('Database not open');
-    return {
-      rows: db.prepare(sql).all(...parameters as any[]) as R[],
-      numAffectedRows: BigInt(db.changes),
-      insertId: BigInt(db.lastInsertRowId),
-    };
+
+    const result: QueryResult<R> = Sentry.startSpan({ name: sql, op: 'db.query' }, () => {
+      return {
+        rows: db!.prepare(sql).all(...parameters as any[]) as R[],
+        numAffectedRows: BigInt(db!.changes),
+        insertId: BigInt(db!.lastInsertRowId),
+      };
+    });
+
+    return result;
   },
   destroy() {
     db?.close();
