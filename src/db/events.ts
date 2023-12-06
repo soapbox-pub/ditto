a6bf5f47cd324dafebb6056943cfe168fa23023e
Alex Gleason <alex@alexgleason.me>
2023-12-05 18:22:12 -0600

    db/events: fix `limit` not being applied correctly

--- src/db/events.ts
+++ src/db/events.ts
@@ -139,8 +139,8 @@ function getFilterQuery(filter: DittoFilter) {
 /** Combine filter queries into a single union query. */
 function getFiltersQuery(filters: DittoFilter[]) {
   return filters
-    .map(getFilterQuery)
-    .reduce((result, query) => result.union(query));
+    .map((filter) => db.selectFrom(() => getFilterQuery(filter).as('events')).selectAll())
+    .reduce((result, query) => result.unionAll(query));
 }
 
 /** Get events for filters from the database. */
