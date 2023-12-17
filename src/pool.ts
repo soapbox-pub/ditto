cdfb21caa62cceae9767fbc7a8e486974ed0f292
Alex Gleason <alex@alexgleason.me>
2023-12-17 11:00:04 -0600

    Pool: log number of active relays to connect to

--- src/pool.ts
+++ src/pool.ts
@@ -1,9 +1,11 @@
 import { getActiveRelays } from '@/db/relays.ts';
 import { type Event, RelayPool } from '@/deps.ts';
 
-const allRelays = await getActiveRelays();
+const activeRelays = await getActiveRelays();
 
-const pool = new RelayPool(allRelays, {
+console.log(`pool: connecting to ${activeRelays.length} relays.`);
+
+const pool = new RelayPool(activeRelays, {
   // The pipeline verifies events.
   skipVerification: true,
   // The logging feature overwhelms the CPU and creates too many logs.
@@ -11,8 +13,8 @@ const pool = new RelayPool(allRelays, {
 });
 
 /** Publish an event to the given relays, or the entire pool. */
-function publish(event: Event, relays: string[] = allRelays) {
+function publish(event: Event, relays: string[] = activeRelays) {
   return pool.publish(event, relays);
 }
 
-export { allRelays, pool, publish };
+export { activeRelays, pool, publish };
