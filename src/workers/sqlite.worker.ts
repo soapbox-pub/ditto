7a38cfbc40c4e6dc9a58f16301f2205359cce07f
Alex Gleason <alex@alexgleason.me>
2024-02-17 14:34:38 -0600

    Remove Sentry from SqliteWorker It seems to be destroying CPU performance?

--- src/workers/sqlite.worker.ts
+++ src/workers/sqlite.worker.ts
@@ -1,6 +1,6 @@
 /// <reference lib="webworker" />
 
-import { Comlink, type CompiledQuery, Debug, DenoSqlite3, type QueryResult, Sentry } from '@/deps.ts';
+import { Comlink, type CompiledQuery, Debug, DenoSqlite3, type QueryResult } from '@/deps.ts';
 import '@/sentry.ts';
 
 let db: DenoSqlite3 | undefined;
@@ -13,16 +13,11 @@ export const SqliteWorker = {
   executeQuery<R>({ sql, parameters }: CompiledQuery): QueryResult<R> {
     if (!db) throw new Error('Database not open');
     debug(sql);
-
-    const result: QueryResult<R> = Sentry.startSpan({ name: sql, op: 'db.query' }, () => {
-      return {
-        rows: db!.prepare(sql).all(...parameters as any[]) as R[],
-        numAffectedRows: BigInt(db!.changes),
-        insertId: BigInt(db!.lastInsertRowId),
-      };
-    });
-
-    return result;
+    return {
+      rows: db!.prepare(sql).all(...parameters as any[]) as R[],
+      numAffectedRows: BigInt(db!.changes),
+      insertId: BigInt(db!.lastInsertRowId),
+    };
   },
   destroy() {
     db?.close();
