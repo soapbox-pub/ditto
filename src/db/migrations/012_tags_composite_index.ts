19adb3ab4438ba0754a807d9cd106c3644b188c9
Alex Gleason <alex@alexgleason.me>
2024-03-02 19:21:48 -0600

    Use composite index for tags

---
+++ src/db/migrations/012_tags_composite_index.ts
@@ -0,0 +1,31 @@
+import { Kysely } from '@/deps.ts';
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema.dropIndex('idx_tags_tag').on('tags').execute();
+  await db.schema.dropIndex('idx_tags_value').on('tags').execute();
+
+  await db.schema
+    .createIndex('idx_tags_tag_value')
+    .on('tags')
+    .columns(['tag', 'value'])
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .dropIndex('idx_tags_tag_value')
+    .on('tags')
+    .execute();
+
+  await db.schema
+    .createIndex('idx_tags_tag')
+    .on('tags')
+    .column('tag')
+    .execute();
+
+  await db.schema
+    .createIndex('idx_tags_value')
+    .on('tags')
+    .column('value')
+    .execute();
+}
