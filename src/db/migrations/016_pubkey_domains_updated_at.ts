c0c586b364c575fc33d8079965c3b2a322a90f45
Alex Gleason <alex@alexgleason.me>
2024-04-05 21:41:55 -0500

    Add last_updated_at column to pubkey_domains

---
+++ src/db/migrations/016_pubkey_domains_updated_at.ts
@@ -0,0 +1,12 @@
+import { Kysely } from '@/deps.ts';
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .alterTable('pubkey_domains')
+    .addColumn('last_updated_at', 'integer', (col) => col.notNull().defaultTo(0))
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.alterTable('pubkey_domains').dropColumn('last_updated_at').execute();
+}
