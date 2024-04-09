11f668dc8cf61903c5a12583bc81ea5da7b33512
P. Reis <patrickpereirareal1@gmail.com>
2024-04-09 15:08:07 -0300

    refactor(interfaces): resolve import specifier via the active import map

--- src/interfaces/DittoFilter.ts
+++ src/interfaces/DittoFilter.ts
@@ -1,6 +1,6 @@
 import { type NostrEvent } from '@/deps.ts';
 
-import { type DittoEvent } from './DittoEvent.ts';
+import { type DittoEvent } from '@/interfaces/DittoEvent.ts';
 
 /** Additional properties that may be added by Ditto to events. */
 export type DittoRelation = Exclude<keyof DittoEvent, keyof NostrEvent>;
