1cb53bdb849d96f83e726e3b474e4ef55e52e529
Alex Gleason <alex@alexgleason.me>
2024-04-19 14:37:53 +0000

    Revert "Merge branch 'rework-cache' into 'main'"
    This reverts merge request !156

---
+++ src/utils/expiring-cache.test.ts
@@ -0,0 +1,18 @@
+import { assert } from '@/deps-test.ts';
+
+import ExpiringCache from './expiring-cache.ts';
+
+Deno.test('ExpiringCache', async () => {
+  const cache = new ExpiringCache(await caches.open('test'));
+
+  await cache.putExpiring('http://mostr.local/1', new Response('hello world'), 300);
+  await cache.putExpiring('http://mostr.local/2', new Response('hello world'), -1);
+
+  // const resp1 = await cache.match('http://mostr.local/1');
+  const resp2 = await cache.match('http://mostr.local/2');
+
+  // assert(resp1!.headers.get('Expires'));
+  assert(!resp2);
+
+  // await resp1!.text();
+});
