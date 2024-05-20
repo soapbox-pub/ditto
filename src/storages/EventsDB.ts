7b099ee5659f278dfe49802e79c9e878ae4d69a1
Alex Gleason <alex@alexgleason.me>
2024-05-20 11:39:31 -0500

    EventsDB: don't index the user's bio for kind 0 events

--- src/storages/EventsDB.ts
+++ src/storages/EventsDB.ts
@@ -180,8 +180,8 @@ class EventsDB implements NStore {
 
   /** Build search content for a user. */
   static buildUserSearchContent(event: NostrEvent): string {
-    const { name, nip05, about } = n.json().pipe(n.metadata()).catch({}).parse(event.content);
-    return [name, nip05, about].filter(Boolean).join('\n');
+    const { name, nip05 } = n.json().pipe(n.metadata()).catch({}).parse(event.content);
+    return [name, nip05].filter(Boolean).join('\n');
   }
 
   /** Build search content from tag values. */
