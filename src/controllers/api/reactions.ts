b0a53f47891a8c30f9a4e5bbb4521f6799a31420
Alex Gleason <alex@alexgleason.me>
2024-05-22 18:33:00 -0500

    Add delete reaction controller

--- src/controllers/api/reactions.ts
+++ src/controllers/api/reactions.ts
@@ -25,7 +25,7 @@ const reactionController: AppController = async (c) => {
 
   await createEvent({
     kind: 7,
-    content: '',
+    content: emoji,
     created_at: Math.floor(Date.now() / 1000),
     tags: [['e', id]],
   }, c);
@@ -35,4 +35,47 @@ const reactionController: AppController = async (c) => {
   return c.json(status);
 };
 
-export { reactionController };
+/**
+ * Delete reactions to a status.
+ * https://docs.pleroma.social/backend/development/API/pleroma_api/#delete-apiv1pleromastatusesidreactionsemoji
+ */
+const deleteReactionController: AppController = async (c) => {
+  const id = c.req.param('id');
+  const emoji = c.req.param('emoji');
+  const signer = c.get('signer')!;
+  const pubkey = await signer.getPublicKey();
+  const store = await Storages.db();
+
+  if (!/^\p{RGI_Emoji}$/v.test(emoji)) {
+    return c.json({ error: 'Invalid emoji' }, 400);
+  }
+
+  const [event] = await store.query([
+    { kinds: [1], ids: [id], limit: 1 },
+  ]);
+
+  if (!event) {
+    return c.json({ error: 'Status not found' }, 404);
+  }
+
+  const events = await store.query([
+    { kinds: [7], authors: [pubkey], '#e': [id] },
+  ]);
+
+  const tags = events
+    .filter((event) => event.content === emoji)
+    .map((event) => ['e', event.id]);
+
+  await createEvent({
+    kind: 5,
+    content: '',
+    created_at: Math.floor(Date.now() / 1000),
+    tags,
+  }, c);
+
+  const status = renderStatus(event, { viewerPubkey: pubkey });
+
+  return c.json(status);
+};
+
+export { deleteReactionController, reactionController };
