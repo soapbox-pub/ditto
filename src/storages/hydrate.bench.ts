1ffc1fc8f40cc81e4ae9f1943bbb948771b5c0d1
Alex Gleason <alex@alexgleason.me>
2024-05-25 10:49:09 -0500

    hydrate.bench: move the `events` variable above the bench

--- src/storages/hydrate.bench.ts
+++ src/storages/hydrate.bench.ts
@@ -4,12 +4,10 @@ import { jsonlEvents } from '@/test.ts';
 const testEvents = await jsonlEvents('fixtures/hydrated.jsonl');
 const testStats = JSON.parse(await Deno.readTextFile('fixtures/stats.json'));
 
-Deno.bench('assembleEvents with home feed', (b) => {
-  // The first 20 events in this file are my home feed.
-  // The rest are events that would be hydrated by the store.
-  const events = testEvents.slice(0, 20);
+// The first 20 events in this file are my home feed.
+// The rest are events that would be hydrated by the store.
+const events = testEvents.slice(0, 20);
 
-  b.start();
-
+Deno.bench('assembleEvents with home feed', () => {
   assembleEvents(events, testEvents, testStats);
 });
