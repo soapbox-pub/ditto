4330cae62610022a7d875d4c077a722a3f2c5808
Alex Gleason <alex@alexgleason.me>
2024-05-24 20:16:51 -0500

    Fix Conf.db in migrations

--- src/db/migrations/002_events_fts.ts
+++ src/db/migrations/002_events_fts.ts
@@ -3,7 +3,7 @@ import { Kysely, sql } from 'kysely';
 import { Conf } from '@/config.ts';
 
 export async function up(db: Kysely<any>): Promise<void> {
-  if (Conf.databaseUrl.protocol === 'sqlite:') {
+  if (Conf.db.dialect === 'sqlite') {
     await sql`CREATE VIRTUAL TABLE events_fts USING fts5(id, content)`.execute(db);
   }
 }
