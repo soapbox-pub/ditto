c55cd2a97743c48e68c56eeea631a49579f6d30c
Alex Gleason <alex@alexgleason.me>
2024-05-26 20:41:14 -0500

    blob -> bytea

--- src/db/migrations/023_add_connections.ts
+++ src/db/migrations/023_add_connections.ts
@@ -5,7 +5,7 @@ export async function up(db: Kysely<any>): Promise<void> {
     .createTable('connections')
     .addColumn('api_token', 'text', (col) => col.primaryKey().unique().notNull())
     .addColumn('user_pubkey', 'text', (col) => col.notNull())
-    .addColumn('server_seckey', 'blob', (col) => col.notNull())
+    .addColumn('server_seckey', 'bytea', (col) => col.notNull())
     .addColumn('server_pubkey', 'text', (col) => col.notNull())
     .addColumn('relays', 'text', (col) => col.defaultTo('[]'))
     .addColumn('connected_at', 'timestamp', (col) => col.defaultTo(sql`CURRENT_TIMESTAMP`))
