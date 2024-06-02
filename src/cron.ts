c969738736186bd00efeaf6dab2b8a5871d8f9e2
Alex Gleason <alex@alexgleason.me>
2024-06-02 13:15:05 -0500

    Save trending hashtags as labels

--- src/cron.ts
+++ src/cron.ts
@@ -5,6 +5,7 @@ import { getTrendingNotes } from '@/trends/trending-notes.ts';
 import { Time } from '@/utils/time.ts';
 import { AdminSigner } from '@/signers/AdminSigner.ts';
 import { handleEvent } from '@/pipeline.ts';
+import { getTrendingHashtags } from '@/trends/trending-hashtags.ts';
 
 const console = new Stickynotes('ditto:trends');
 
@@ -32,7 +33,32 @@ async function updateTrendingNotesCache() {
   console.info('Trending notes cache updated.');
 }
 
+async function updateTrendingHashtagsCache() {
+  console.info('Updating trending hashtags cache...');
+  const kysely = await DittoDB.getInstance();
+  const yesterday = Math.floor((Date.now() - Time.days(1)) / 1000);
+  const signal = AbortSignal.timeout(1000);
+
+  const hashtags = await getTrendingHashtags(kysely, { since: yesterday, limit: 20, threshold: 3 });
+  const signer = new AdminSigner();
+
+  const label = await signer.signEvent({
+    kind: 1985,
+    content: '',
+    tags: [
+      ['L', 'pub.ditto.trends'],
+      ['l', 'hashtags', 'pub.ditto.trends'],
+      ...hashtags.map(({ tag }) => ['t', tag]),
+    ],
+    created_at: Math.floor(Date.now() / 1000),
+  });
+
+  await handleEvent(label, signal);
+  console.info('Trending hashtags cache updated.');
+}
+
 /** Start cron jobs for the application. */
 export function cron() {
   Deno.cron('update trending notes cache', { minute: { every: 15 } }, updateTrendingNotesCache);
+  Deno.cron('update trending hashtags cache', { dayOfMonth: { every: 1 } }, updateTrendingHashtagsCache);
 }
