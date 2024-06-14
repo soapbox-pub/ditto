7186f49316061c66b27ae119b3dee7fe1e488da1
Alex Gleason <alex@alexgleason.me>
2024-06-13 19:17:53 -0500

    Remove URL restriction from r tag

--- src/storages/EventsDB.ts
+++ src/storages/EventsDB.ts
@@ -38,7 +38,7 @@ class EventsDB implements NStore {
     'p': ({ event, count, value }) => (count < 15 || event.kind === 3) && isNostrId(value),
     'proxy': ({ count, value }) => count === 0 && isURL(value),
     'q': ({ event, count, value }) => count === 0 && event.kind === 1 && isNostrId(value),
-    'r': ({ event, count, value }) => (event.kind === 1985 ? count < 20 : count < 3) && isURL(value),
+    'r': ({ event, count }) => (event.kind === 1985 ? count < 20 : count < 3),
     't': ({ event, count, value }) => (event.kind === 1985 ? count < 20 : count < 5) && value.length < 50,
   };
 
