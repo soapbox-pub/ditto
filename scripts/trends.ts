8d8e46eae80e099067228fa4ba2328fc23433a96
Alex Gleason <alex@alexgleason.me>
2024-06-15 11:51:55 -0500

    Add a script to update trends

---
+++ scripts/trends.ts
@@ -0,0 +1,44 @@
+import { z } from 'zod';
+
+import {
+  updateTrendingEvents,
+  updateTrendingHashtags,
+  updateTrendingLinks,
+  updateTrendingPubkeys,
+  updateTrendingZappedEvents,
+} from '@/trends.ts';
+
+const trendSchema = z.enum(['pubkeys', 'zapped_events', 'events', 'hashtags', 'links']);
+const trends = trendSchema.array().parse(Deno.args);
+
+if (!trends.length) {
+  trends.push('pubkeys', 'zapped_events', 'events', 'hashtags', 'links');
+}
+
+for (const trend of trends) {
+  switch (trend) {
+    case 'pubkeys':
+      console.log('Updating trending pubkeys...');
+      await updateTrendingPubkeys();
+      break;
+    case 'zapped_events':
+      console.log('Updating trending zapped events...');
+      await updateTrendingZappedEvents();
+      break;
+    case 'events':
+      console.log('Updating trending events...');
+      await updateTrendingEvents();
+      break;
+    case 'hashtags':
+      console.log('Updating trending hashtags...');
+      await updateTrendingHashtags();
+      break;
+    case 'links':
+      console.log('Updating trending links...');
+      await updateTrendingLinks();
+      break;
+  }
+}
+
+console.log('Trends updated.');
+Deno.exit(0);
