98565efa12a5e06533a7b81e5a6297ec81502e9a
P. Reis <patrickpereirareal1@gmail.com>
2024-06-17 19:49:18 -0300

    refactor(bolt11): get amount with new library

--- src/utils/bolt11.ts
+++ src/utils/bolt11.ts
@@ -1,14 +1,15 @@
-import bolt11 from 'bolt11';
+import bolt11 from 'light-bolt11-decoder';
 
 /** Decodes the invoice and returns the amount in millisatoshis */
 function getAmount(invoice: string | undefined): string | undefined {
   if (!invoice) return;
 
   try {
-    const decoded = bolt11.decode(invoice);
-    return decoded?.millisatoshis ?? undefined;
-  } catch (e) {
-    console.log(e);
+    const amount = (bolt11.decode(invoice).sections as { name: string; value: string }[]).find(
+      ({ name }) => name === 'amount',
+    )?.value;
+    return amount;
+  } catch {
     return;
   }
 }
