0d3c6192459d17354f7b45b33c0c4f21ced5c8d0
Alex Gleason <alex@alexgleason.me>
2024-06-18 15:24:44 -0500

    rateLimitMiddleware: improve tsdoc comment

--- src/middleware/rateLimitMiddleware.ts
+++ src/middleware/rateLimitMiddleware.ts
@@ -1,8 +1,11 @@
 import { MiddlewareHandler } from '@hono/hono';
 import { rateLimiter } from 'hono-rate-limiter';
 
-/** Rate limit middleware for Hono. */
+/**
+ * Rate limit middleware for Hono, based on [`hono-rate-limiter`](https://github.com/rhinobase/hono-rate-limiter).
+ */
 export function rateLimitMiddleware(limit: number, windowMs: number): MiddlewareHandler {
+  // @ts-ignore Mismatched hono versions.
   return rateLimiter({
     limit,
     windowMs,
