875eee7a77e668bdd932034e8f5ef73dad276b8e
Alex Gleason <alex@alexgleason.me>
2024-06-19 13:25:06 -0500

    Avoid using the user's own relay list when publishing events
    Sadly I think these relays are causing Ditto to lock up and stop working. This sucks

--- src/utils/outbox.ts
+++ src/utils/outbox.ts
@@ -2,11 +2,11 @@ import { NStore } from '@nostrify/nostrify';
 
 import { Conf } from '@/config.ts';
 
-export async function getRelays(store: NStore, pubkey: string): Promise<Set<string>> {
+export async function getRelays(store: NStore, _pubkey: string): Promise<Set<string>> {
   const relays = new Set<`wss://${string}`>();
 
   const events = await store.query([
-    { kinds: [10002], authors: [pubkey, Conf.pubkey], limit: 2 },
+    { kinds: [10002], authors: [/*pubkey, */ Conf.pubkey], limit: 2 },
   ]);
 
   for (const event of events) {
