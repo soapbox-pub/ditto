1698c1b19b30339092259133708e8e719dde2ce9
P. Reis <patrickpereirareal1@gmail.com>
2024-07-24 10:54:20 -0300

    feat: create startup file

---
+++ src/startup.ts
@@ -0,0 +1,16 @@
+// Starts up applications required to run before the HTTP server is on.
+
+import { Conf } from '@/config.ts';
+import { createZapSplitsIfNotExists } from '@/utils/zap-split.ts';
+import { cron } from '@/cron.ts';
+import { startFirehose } from '@/firehose.ts';
+
+if (Conf.firehoseEnabled) {
+  startFirehose();
+}
+
+if (Conf.cronEnabled) {
+  cron();
+}
+
+await createZapSplitsIfNotExists();
