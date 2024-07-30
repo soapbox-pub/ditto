ce18cb928a68f00408d9036f66a8ac9a53748488
Alex Gleason <alex@alexgleason.me>
2024-07-29 21:19:44 -0500

    KyselyLogger: also log errors

--- src/db/KyselyLogger.ts
+++ src/db/KyselyLogger.ts
@@ -4,18 +4,16 @@ import { dbQueryTimeHistogram } from '@/metrics.ts';
 
 /** Log the SQL for queries. */
 export const KyselyLogger: Logger = (event) => {
-  if (event.level === 'query') {
-    const console = new Stickynotes('ditto:sql');
+  const console = new Stickynotes('ditto:sql');
 
-    const { query, queryDurationMillis } = event;
-    const { sql, parameters } = query;
+  const { query, queryDurationMillis } = event;
+  const { sql, parameters } = query;
 
-    dbQueryTimeHistogram.observe(queryDurationMillis);
+  dbQueryTimeHistogram.observe(queryDurationMillis);
 
-    console.debug(
-      sql,
-      JSON.stringify(parameters),
-      `\x1b[90m(${(queryDurationMillis / 1000).toFixed(2)}s)\x1b[0m`,
-    );
-  }
+  console.debug(
+    sql,
+    JSON.stringify(parameters),
+    `\x1b[90m(${(queryDurationMillis / 1000).toFixed(2)}s)\x1b[0m`,
+  );
 };
