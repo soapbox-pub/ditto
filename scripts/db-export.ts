ba3f81695512ebf27675546dd8f2bd581a967540
Alex Gleason <alex@alexgleason.me>
2024-08-02 16:54:35 -0500

    Add db:import task

--- scripts/db-export.ts
+++ scripts/db-export.ts
@@ -4,13 +4,21 @@ const store = await Storages.db();
 
 console.warn('Exporting events...');
 
+let count = 0;
+
 for await (const msg of store.req([{}])) {
-  if (msg[0] === 'EVENT') console.log(JSON.stringify(msg[2]));
-  if (msg[0] === 'EOSE') break;
+  if (msg[0] === 'EOSE') {
+    break;
+  }
+  if (msg[0] === 'EVENT') {
+    console.log(JSON.stringify(msg[2]));
+    count++;
+  }
   if (msg[0] === 'CLOSED') {
     console.error('Database closed unexpectedly');
     break;
   }
 }
 
-console.warn('Done!');
+console.warn(`Exported ${count} events`);
+Deno.exit();
