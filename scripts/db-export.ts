9298bf66081a8f0f037dd84005275c15ce2d90f9
Siddharth S Singh <simsid66@gmail.com>
2024-08-23 16:25:34 +0530

    first commit (nonfunctional)

--- scripts/db-export.ts
+++ scripts/db-export.ts
@@ -1,24 +1,43 @@
 import { Storages } from '@/storages.ts';
+import { Command } from 'commander';
 
 const store = await Storages.db();
 
-console.warn('Exporting events...');
-
-let count = 0;
-
-for await (const msg of store.req([{}])) {
-  if (msg[0] === 'EOSE') {
-    break;
-  }
-  if (msg[0] === 'EVENT') {
-    console.log(JSON.stringify(msg[2]));
-    count++;
-  }
-  if (msg[0] === 'CLOSED') {
-    console.error('Database closed unexpectedly');
-    break;
-  }
+interface ExportFilter {
+  authors: string[];
 }
 
-console.warn(`Exported ${count} events`);
+if (import.meta.main) {
+  const exporter = new Command()
+    .name('db:export')
+    .description('Export the specified set of events from the Ditto database.')
+    .version('0.1.0')
+    .showHelpAfterError();
+
+  exporter
+    // .option('')
+    .action(async (args: ExportFilter) => {
+      console.warn('Exporting events...');
+      let count = 0;
+
+      for await (const msg of store.req([{}])) {
+        if (msg[0] === 'EOSE') {
+          break;
+        }
+        if (msg[0] === 'EVENT') {
+          console.log(JSON.stringify(msg[2]));
+          count++;
+        }
+        if (msg[0] === 'CLOSED') {
+          console.error('Database closed unexpectedly');
+          break;
+        }
+      }
+
+      console.warn(`Exported ${count} events`);
+    });
+
+  exporter.parse(Deno.args, { from: 'user' });
+}
+
 Deno.exit();
