d7ae3722c8e3a4557e997ed787e52b99c449b19b
P. Reis <patrickpereirareal1@gmail.com>
2024-09-15 16:52:49 -0300

    refactor: insert each event per iteration in for loop - db:populate-search

--- scripts/db-populate-search.ts
+++ scripts/db-populate-search.ts
@@ -1,11 +1,8 @@
 import { NSchema as n } from '@nostrify/nostrify';
 import { Storages } from '@/storages.ts';
-import { DittoTables } from '@/db/DittoTables.ts';
 
 const store = await Storages.db();
 const kysely = await Storages.kysely();
-
-const values: DittoTables['author_search'][] = [];
 
 for await (const msg of store.req([{ kinds: [0] }])) {
   if (msg[0] === 'EVENT') {
@@ -14,22 +11,22 @@ for await (const msg of store.req([{ kinds: [0] }])) {
     const { name, nip05 } = n.json().pipe(n.metadata()).catch({}).parse(content);
     const search = [name, nip05].filter(Boolean).join(' ').trim();
 
-    values.push({
-      pubkey: pubkey,
-      search,
-    });
+    try {
+      await kysely.insertInto('author_search').values({
+        pubkey,
+        search,
+      }).onConflict(
+        (oc) =>
+          oc.column('pubkey')
+            .doUpdateSet((eb) => ({ search: eb.ref('excluded.search') })),
+      )
+        .execute();
+    } catch {
+      // do nothing
+    }
+  } else {
+    break;
   }
-}
-
-try {
-  await kysely.insertInto('author_search').values(values).onConflict(
-    (oc) =>
-      oc.column('pubkey')
-        .doUpdateSet((eb) => ({ search: eb.ref('excluded.search') })),
-  )
-    .execute();
-} catch {
-  // do nothing
 }
 
 Deno.exit();
