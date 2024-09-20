f0c8096498e7646f596896502877523a07248a96
Alex Gleason <alex@alexgleason.me>
2024-09-20 10:06:17 -0500

    stats64: delete invalid rows first

--- src/db/migrations/036_stats64.ts
+++ src/db/migrations/036_stats64.ts
@@ -1,11 +1,14 @@
-import { Kysely } from 'kysely';
+import { Kysely, sql } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
-  await db.schema.alterTable('author_stats').alterColumn('pubkey', (col) => col.setDataType('char(64)')).execute();
+  await db.deleteFrom('event_stats').where(sql<number>`length(event_id)`, '>', 64).execute();
+  await db.deleteFrom('author_stats').where(sql<number>`length(pubkey)`, '>', 64).execute();
+
   await db.schema.alterTable('event_stats').alterColumn('event_id', (col) => col.setDataType('char(64)')).execute();
+  await db.schema.alterTable('author_stats').alterColumn('pubkey', (col) => col.setDataType('char(64)')).execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await db.schema.alterTable('author_stats').alterColumn('pubkey', (col) => col.setDataType('text')).execute();
   await db.schema.alterTable('event_stats').alterColumn('event_id', (col) => col.setDataType('text')).execute();
+  await db.schema.alterTable('author_stats').alterColumn('pubkey', (col) => col.setDataType('text')).execute();
 }
