1a1adc1654f31da5bc9347c26bfb21e9de599600
P. Reis <patrickpereirareal1@gmail.com>
2024-09-23 14:47:17 -0300

    feat: add ditto:relay in /relay - sticky notes

--- src/controllers/nostr/relay.ts
+++ src/controllers/nostr/relay.ts
@@ -1,3 +1,4 @@
+import Debug from '@soapbox/stickynotes/debug';
 import TTLCache from '@isaacs/ttlcache';
 import {
   NostrClientCLOSE,
@@ -28,6 +29,8 @@ const limiter = new TTLCache<string, number>();
 
 /** Connections for metrics purposes. */
 const connections = new Set<WebSocket>();
+
+const debug = Debug('ditto:relay');
 
 /** Set up the Websocket connection. */
 function connectStream(socket: WebSocket, ip: string | undefined) {
@@ -141,7 +144,7 @@ function connectStream(socket: WebSocket, ip: string | undefined) {
         send(['OK', event.id, false, e.message]);
       } else {
         send(['OK', event.id, false, 'error: something went wrong']);
-        console.error(e);
+        debug(e);
       }
     }
   }
