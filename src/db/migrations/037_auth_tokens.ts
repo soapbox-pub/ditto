ff361a410662ae11993e915493d3a60a698d0378
Alex Gleason <alex@alexgleason.me>
2024-10-02 18:34:19 -0500

    Recreate nip46_tokens in down migration

--- src/db/migrations/037_auth_tokens.ts
+++ src/db/migrations/037_auth_tokens.ts
@@ -49,4 +49,14 @@ export async function up(db: Kysely<DB>): Promise<void> {
 
 export async function down(db: Kysely<DB>): Promise<void> {
   await db.schema.dropTable('auth_tokens').execute();
+
+  await db.schema
+    .createTable('nip46_tokens')
+    .addColumn('api_token', 'text', (col) => col.primaryKey().unique().notNull())
+    .addColumn('user_pubkey', 'text', (col) => col.notNull())
+    .addColumn('server_seckey', 'bytea', (col) => col.notNull())
+    .addColumn('server_pubkey', 'text', (col) => col.notNull())
+    .addColumn('relays', 'text', (col) => col.defaultTo('[]'))
+    .addColumn('connected_at', 'timestamp', (col) => col.defaultTo(sql`CURRENT_TIMESTAMP`))
+    .execute();
 }
