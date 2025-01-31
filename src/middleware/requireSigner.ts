c83a2dba7e99aefac0c4cc930c4a80dd4a08bac0
Alex Gleason <alex@alexgleason.me>
2025-01-30 20:39:51 -0600

    Give requireSigner middleware the right type

--- src/middleware/requireSigner.ts
+++ src/middleware/requireSigner.ts
@@ -1,9 +1,9 @@
+import { MiddlewareHandler } from '@hono/hono';
 import { HTTPException } from '@hono/hono/http-exception';
-
-import { AppMiddleware } from '@/app.ts';
+import { NostrSigner } from '@nostrify/nostrify';
 
 /** Throw a 401 if a signer isn't set. */
-export const requireSigner: AppMiddleware = async (c, next) => {
+export const requireSigner: MiddlewareHandler<{ Variables: { signer: NostrSigner } }> = async (c, next) => {
   if (!c.get('signer')) {
     throw new HTTPException(401, { message: 'No pubkey provided' });
   }
