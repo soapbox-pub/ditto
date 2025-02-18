c29fc57a8cc5bfa76315b3c9c11d12843347e5fa
Alex Gleason <alex@alexgleason.me>
2025-02-18 16:35:45 -0600

    Switch to genEvent from Nostrify

--- packages/ditto/test.ts
+++ packages/ditto/test.ts
@@ -1,35 +1,14 @@
 import { DittoDB } from '@ditto/db';
 import { NostrEvent } from '@nostrify/nostrify';
-import { finalizeEvent, generateSecretKey } from 'nostr-tools';
 
 import { Conf } from '@/config.ts';
 import { EventsDB } from '@/storages/EventsDB.ts';
-import { purifyEvent } from '@/utils/purify.ts';
 import { sql } from 'kysely';
 
 /** Import an event fixture by name in tests. */
 export async function eventFixture(name: string): Promise<NostrEvent> {
   const result = await import(`~/fixtures/events/${name}.json`, { with: { type: 'json' } });
   return structuredClone(result.default);
-}
-
-/** Import a JSONL fixture by name in tests. */
-export async function jsonlEvents(path: string): Promise<NostrEvent[]> {
-  const data = await Deno.readTextFile(path);
-  return data.split('\n').map((line) => JSON.parse(line));
-}
-
-/** Generate an event for use in tests. */
-export function genEvent(t: Partial<NostrEvent> = {}, sk: Uint8Array = generateSecretKey()): NostrEvent {
-  const event = finalizeEvent({
-    kind: 255,
-    created_at: 0,
-    content: '',
-    tags: [],
-    ...t,
-  }, sk);
-
-  return purifyEvent(event);
 }
 
 /** Create a database for testing. It uses `DATABASE_URL`, or creates an in-memory database by default. */
