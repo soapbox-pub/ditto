2150259abad023234cf8a9aa92ee8c59f37eff74
Alex Gleason <alex@alexgleason.me>
2025-02-20 10:36:43 -0600

    languageSchema does not lowercase the code

--- packages/translators/schema.test.ts
+++ packages/translators/schema.test.ts
@@ -3,5 +3,6 @@ import { assertEquals } from '@std/assert';
 import { languageSchema } from './schema.ts';
 
 Deno.test('languageSchema', () => {
-  assertEquals(languageSchema.safeParse('en').success, true);
+  assertEquals(languageSchema.safeParse('pt').success, true);
+  assertEquals(languageSchema.safeParse('PT').success, false);
 });
