084c6aa94443422f40a91537ae09cde7c354da00
Alex Gleason <alex@alexgleason.me>
2025-02-21 21:55:00 -0600

    Fix DummyDB test

--- packages/db/adapters/DummyDB.test.ts
+++ packages/db/adapters/DummyDB.test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from '@std/assert';
 import { DummyDB } from './DummyDB.ts';
 
 Deno.test('DummyDB', async () => {
-  const db = DummyDB.create();
+  const db = new DummyDB();
   const rows = await db.kysely.selectFrom('nostr_events').selectAll().execute();
 
   assertEquals(rows, []);
