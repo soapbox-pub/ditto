4f2aad9c5ddc9efeb2ab305ea98866013c851498
Alex Gleason <alex@alexgleason.me>
2025-02-27 23:42:29 -0600

    Export stuff from transcode/mod.ts

--- packages/transcode/mod.ts
+++ packages/transcode/mod.ts
@@ -1,2 +1,5 @@
+export { getVideoDimensions } from './analyze.ts';
 export { ffmpeg, type FFmpegFlags } from './ffmpeg.ts';
+export { ffprobe, type FFprobeFlags } from './ffprobe.ts';
+export { extractVideoFrame } from './frame.ts';
 export { transcodeVideo } from './transcode.ts';
