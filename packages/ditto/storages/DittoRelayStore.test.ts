b8b6174fcc146b60ab032b25d5f9bb2f1b384306
Alex Gleason <alex@alexgleason.me>
2025-03-03 15:24:26 -0600

    Test that DittoRelayStore generates nip05 request set event

--- packages/ditto/storages/DittoRelayStore.test.ts
+++ packages/ditto/storages/DittoRelayStore.test.ts
@@ -2,11 +2,38 @@ import { DittoPolyPg } from '@ditto/db';
 import { DittoConf } from '@ditto/conf';
 import { genEvent, MockRelay } from '@nostrify/nostrify/test';
 import { assertEquals } from '@std/assert';
+import { waitFor } from '@std/async/unstable-wait-for';
 import { generateSecretKey, getPublicKey } from 'nostr-tools';
 
 import { DittoRelayStore } from './DittoRelayStore.ts';
 
 import type { NostrMetadata } from '@nostrify/types';
+
+Deno.test('generates set event for nip05 request', async () => {
+  await using test = setupTest();
+
+  const admin = await test.conf.signer.getPublicKey();
+  const event = genEvent({ kind: 3036, tags: [['r', 'alex@gleasonator.dev'], ['p', admin]] });
+
+  await test.store.event(event);
+
+  const filter = { kinds: [30383], authors: [admin], '#d': [event.id] };
+
+  await waitFor(async () => {
+    const { count } = await test.store.count([filter]);
+    return count > 0;
+  }, 3000);
+
+  const [result] = await test.store.query([filter]);
+
+  assertEquals(result?.tags, [
+    ['d', event.id],
+    ['p', event.pubkey],
+    ['k', '3036'],
+    ['r', 'alex@gleasonator.dev'],
+    ['n', 'pending'],
+  ]);
+});
 
 Deno.test('updateAuthorData sets nip05', async () => {
   const alex = generateSecretKey();
@@ -38,20 +65,25 @@ Deno.test('updateAuthorData sets nip05', async () => {
   assertEquals(row?.nip05_hostname, 'gleasonator.dev');
 });
 
-function setupTest(cb: (req: Request) => Response | Promise<Response>) {
+function setupTest(cb?: (req: Request) => Response | Promise<Response>) {
   const conf = new DittoConf(Deno.env);
   const db = new DittoPolyPg(conf.databaseUrl);
   const relay = new MockRelay();
 
   const mockFetch: typeof fetch = async (input, init) => {
     const req = new Request(input, init);
-    return await cb(req);
+    if (cb) {
+      return await cb(req);
+    } else {
+      return new Response('Not mocked', { status: 404 });
+    }
   };
 
   const store = new DittoRelayStore({ conf, db, relay, fetch: mockFetch });
 
   return {
     db,
+    conf,
     store,
     [Symbol.asyncDispose]: async () => {
       await store[Symbol.asyncDispose]();
