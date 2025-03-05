9b422d8e31547d05ec80c325e5e8951d1c957f5a
Alex Gleason <alex@alexgleason.me>
2025-03-05 14:47:35 -0600

    Add note about hanging queries in DittoPostgres test

--- packages/db/adapters/DittoPostgres.test.ts
+++ packages/db/adapters/DittoPostgres.test.ts
@@ -9,3 +9,14 @@ Deno.test('DittoPostgres', { ignore: !isPostgres }, async () => {
   await using db = new DittoPostgres(conf.databaseUrl);
   await db.migrate();
 });
+
+// FIXME: There is a problem with postgres-js where queries just hang after the database is closed.
+
+// Deno.test('DittoPostgres query after closing', { ignore: !isPostgres }, async () => {
+//   const db = new DittoPostgres(conf.databaseUrl);
+//   await db[Symbol.asyncDispose]();
+//
+//   await assertRejects(
+//     () => db.kysely.selectFrom('nostr_events').selectAll().execute(),
+//   );
+// });
