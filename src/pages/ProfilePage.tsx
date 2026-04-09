437b8de65238dd756103b2e4d28c69912563a9b8
Alex Gleason <alex@alexgleason.me>
2026-04-08 23:34:03 -0500

    Remove right sidebar content and show profile fields inline

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -2269,9 +2269,9 @@ type EditableTab = { label: string; isCore: boolean; tab?: ProfileTab };
                 </div>
               )}
 
-              {/* Profile fields shown inline on mobile (sidebar is hidden below xl) */}
+              {/* Profile fields shown inline */}
               {fields.length > 0 && (
-                <div className="mt-4 space-y-3 xl:hidden">
+                <div className="mt-4 space-y-3">
                   {fields.map((field, i) => (
                     <ProfileFieldInline key={i} field={field} />
                   ))}
